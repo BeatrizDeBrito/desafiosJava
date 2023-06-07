a03a0f2b05b24f64b057bd77cbf9b3a94ff120b6
BeatrizDeBrito <beatriz.de.brito@hotmail.com>
2023-06-06 22:06:53 -0300

    Refatora o nome da classe principal

--- src/hipotenusa/Main.java
+++ src/hipotenusa/Main.java
@@ -2,7 +2,7 @@ package hipotenusa;
 
 import java.util.Scanner;
 
-public class Hypotenuse {
+public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
